37b7e4a5fc9cd3cc392a0be171e7a3bc2996b2c1
joel-g <joelaguerra1@gmail.com>
2017-04-18 21:46:53 -0700

    do release 1 (interview questions)

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+employee = gets.chomp
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+birthyear = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic = gets.chomp
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
