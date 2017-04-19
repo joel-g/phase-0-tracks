e0e16c4268dbc5ebe7e354e6b9041e3c0106e55a
joel-g <joelaguerra1@gmail.com>
2017-04-19 12:56:11 -0700

    add logic to vampires

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,5 +6,21 @@ puts "What year were you born?"
 birthyear = gets.chomp.to_i
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic = gets.chomp
+if garlic == "y"
+	garlic = true
+else garlic = false
+end
 puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+if insurance == "y"
+	insurance = true
+else insurance = false
+end
+if 2017 - age == birthyear && garlic == true || insurance == true
+	vampire = "Probably not a vampire."
+elsif 2017 - age == birthyear && garlic == false || insurance == false
+	vampire = "Probably a vampire."
+elsif 2017 - age !== birthyear && garlic == false && insurance == false)
+	vampire = "Almost certainly a vampire."
+end
+puts vampire
