3eed0335f1d889f5cca207ecbd9467fb9ce2cac0
Joel Guerra <joelaguerra1@gmail.com>
2017-04-19 19:41:30 -0700

    complete vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,26 +9,34 @@ num_employees.times do
 	age = gets.chomp.to_i
 	puts "What year were you born?"
 	birthyear = gets.chomp.to_i
+		if 2017 - age == birthyear
+			agecorrect = true
+		else
+			agecorrect = false
+		end
 	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 	garlic = gets.chomp
-	if garlic == "y"
-		garlic = true
-	else garlic = false
-	end
+		if garlic == "y"
+			garlic = true
+		else garlic = false
+		end
 	puts "Would you like to enroll in the company's health insurance?"
 	insurance = gets.chomp
-	if insurance == "y"
-		insurance = true
-	else insurance = false
-	end
-
-	if 2017 - age == birthyear
-		agecorrect = true
-	else
-		agecorrect = false
-	end
-
-	if employee == "Drake Cula" || employee == "Tu Fang"
+		if insurance == "y"
+			insurance = true
+		else insurance = false
+		end
+	allergy = nil
+	until allergy == "done"
+		puts "Please list an allergy you have. If no more allergies type 'done'"
+		allergy = gets.chomp
+		if allergy == "sunshine"
+			sunshine = true
+		end
+	end	
+	if sunshine == true
+		vampire = "Probably a vampire"
+	elsif employee == "Drake Cula" || employee == "Tu Fang"
 		vampire = "Definitely a vampire"
 	elsif agecorrect == true && (garlic == true || insurance == true)
 		vampire = "Probably not a vampire."
@@ -40,9 +48,5 @@ num_employees.times do
 		vampire = "Results inconclusive"
 	end
 	puts vampire
-
-	puts agecorrect
-	puts garlic
-	puts insurance
-
 end
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends"
