b6dd6afdf7532cd063a1384f7bd1da46011591d5
Joel Guerra <joelaguerra1@gmail.com>
2017-04-19 19:23:36 -0700

    complete release 2 and 3

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,26 +1,48 @@
-puts "What is your name?"
-employee = gets.chomp
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "What year were you born?"
-birthyear = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-garlic = gets.chomp
-if garlic == "y"
-	garlic = true
-else garlic = false
+puts "How many employees are you processing?"
+num_employees = gets.chomp.to_i
+
+num_employees.times do
+		
+	puts "What is your name?"
+	employee = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp.to_i
+	puts "What year were you born?"
+	birthyear = gets.chomp.to_i
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+	garlic = gets.chomp
+	if garlic == "y"
+		garlic = true
+	else garlic = false
+	end
+	puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp
+	if insurance == "y"
+		insurance = true
+	else insurance = false
+	end
+
+	if 2017 - age == birthyear
+		agecorrect = true
+	else
+		agecorrect = false
+	end
+
+	if employee == "Drake Cula" || employee == "Tu Fang"
+		vampire = "Definitely a vampire"
+	elsif agecorrect == true && (garlic == true || insurance == true)
+		vampire = "Probably not a vampire."
+	elsif agecorrect == false && garlic == false && insurance == false
+		vampire = "Almost certainly a vampire."
+	elsif agecorrect == false && garlic == false || insurance == false
+		vampire = "Probably a vampire."
+	else
+		vampire = "Results inconclusive"
+	end
+	puts vampire
+
+	puts agecorrect
+	puts garlic
+	puts insurance
+
 end
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
-if insurance == "y"
-	insurance = true
-else insurance = false
-end
-if 2017 - age == birthyear && garlic == true || insurance == true
-	vampire = "Probably not a vampire."
-elsif 2017 - age == birthyear && garlic == false || insurance == false
-	vampire = "Probably a vampire."
-elsif 2017 - age !== birthyear && garlic == false && insurance == false)
-	vampire = "Almost certainly a vampire."
-end
-puts vampire
