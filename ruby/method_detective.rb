914c697c9e4455fef3ce03b6c014233e8b9bc36b
Joel Guerra <joelaguerra1@gmail.com>
2017-04-24 19:16:45 -0700

    complete 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -7,33 +7,35 @@
 "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
 "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+"the usual".concat(" suspects")
+"the usual" << " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+" suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter".slice(1-39)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".split.join(" ")
+"Elementary,    my   dear        Watson!".squeeze(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".unpack('C')
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (What is the significance of the number 122 in relation to the character z?) 122 is z's 8 bit unsigned character (ASCII)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count("a")
 # => 4
