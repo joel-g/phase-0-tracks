8176aa2d0b9fc8d31ed5f4abe584b768acea0c54
joel-g <joelaguerra1@gmail.com>
2017-04-25 09:51:11 -0700

    add questions re: design customer

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,5 +1,25 @@
-#gather info from user
+#gather info from user (name, age, num children, decore theme, house sq ft)
 
 #convert input to needed data types
 
 #get updates
+
+customer = {
+
+}
+
+puts "What is the new client's full name?"
+customer[:fullname] = gets.chomp
+
+puts "What is customer's age?"
+customer[:age] = gets.chomp.to_i
+
+puts "How many children does the customer have?"
+customer[:kid_count] = gets.chomp.to_i
+
+puts "What is customer's decore theme?"
+customer[:decore_theme] = gets.chomp
+
+puts "How many square feet is the home?"
+customer[:home_size] = gets.chomp.to_i
+
