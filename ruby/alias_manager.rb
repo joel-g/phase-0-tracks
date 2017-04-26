4219db438c0159f64bdbb6cb5b80c8703510794a
Joel Guerra <joelaguerra1@gmail.com>
2017-04-26 15:25:35 -0700

    basis of alias method in alias_manager.rb

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,35 @@
+#swap first name
+
+#down case everything
+
+#split into two strings
+
+#change vowels to next vowel (aeiou) - compare against vowel string
+
+#change the consonants to the next consanant - compare against consanant string
+
+#deal with Z
+
+#join
+
+#capitalize
+
+name = gets.chomp
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+def alias
+name = name.downcase.split(' ').reverse
+first = name[0].chars
+last = name[1].chars
+
+end
+end
+
+def vowelizer
+
+end
+
+
+
+p first
+p last
