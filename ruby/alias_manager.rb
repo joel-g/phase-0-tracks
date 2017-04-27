68c7a2408e2e036764c498e1549ec048abcbfda5
joel-g <joelaguerra1@gmail.com>
2017-04-26 20:29:44 -0700

    complete alias_manager.rb

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,9 +14,12 @@
 
 #capitalize
 
-#reverse and split full name
-real_name = gets.chomp
+#reverse and split full namel
 
+
+
+
+#NEXT_VOWEL method swaps vowels for next vowel
 def next_vowel(input) 
 x = 0
 input.length.times do
@@ -38,17 +41,42 @@ input.length.times do
 return input
 end
 
+#NEXT_CONS method swaps consonants for next consanant
 def next_cons(input)
 x = 0
 input.length.times do
+	input[x].gsub!('z','b')
+	input[x].gsub!('y','z')
+	input[x].gsub!('x','y')
+	input[x].gsub!('w','x')
+	input[x].gsub!('v','w')
+	input[x].gsub!('t','v')
+	input[x].gsub!('s','t')
+	input[x].gsub!('r','s')
+	input[x].gsub!('q','r')
+	input[x].gsub!('p','q')
+	input[x].gsub!('n','p')
+	input[x].gsub!('m','n')
+	input[x].gsub!('l','m')
+	input[x].gsub!('k','l')
 	input[x].gsub!('j','k')
+	input[x].gsub!('h','j')
+	input[x].gsub!('g','h')
+	input[x].gsub!('f','g')
+	input[x].gsub!('d','f')
+	input[x].gsub!('c','d')
+	input[x].gsub!('b','c')
 x += 1
 end
 return input
 end
 
-def aliasizer(rn)
-split_name = rn.downcase.split(' ').reverse
+# This method splits name full name into
+# first and last names and then swaps them after removing caps.
+# Then runs the Vowel and Cons methods 
+# Then rejoins the letters and words into a name
+def aliasizer(input)
+split_name = input.downcase.split(' ').reverse
 first_name = next_vowel(split_name[0].chars)
 last_name = next_vowel(split_name[1].chars)
 first_name = next_cons(first_name)
@@ -57,25 +85,25 @@ new_name = first_name.join.capitalize + " " + last_name.join.capitalize
 return new_name
 end
 
-p aliasizer(real_name)
+# blank starting values
+real_name = ''
+agent_list = []
 
-# x = 0
-# last_name.length.times do
-# case last_name[x]
-# when "a"
-# 	last_name[x] = "e"
-# when "e"
-# 	last_name[x] = "i"
-# when "i"
-# 	last_name[x] = "o"
-# when "o"
-# 	last_name[x] = "u"
-# when "u"
-# 	last_name[x] = "a"
-# else 
-# end
-# x += 1
-# end
+#User interface
+until
+puts "What agent name would you like to get an alias for? (type 'quit' to quit)"
+real_name = gets.chomp
+break if real_name == "quit"
+agent_list.push(real_name, aliasizer(real_name))
+end
 
-# p first_name
-# p last_name
+# Out puts the entire array of agents
+real = 0
+fake = 1
+loop_timer = agent_list.length
+loop_timer = loop_timer / 2
+loop_timer.times do
+puts "#{agent_list[real]} is also known as #{agent_list[fake]}."
+real += 2
+fake += 2
+end
