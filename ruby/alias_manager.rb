e168fd14d2c6b5688ed4e433a3927bc595111b55
joel-g <joelaguerra1@gmail.com>
2017-04-26 18:11:37 -0700

    committing to switches

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,23 +14,54 @@
 
 #capitalize
 
+#reverse and split full name
+real_name = gets.chomp
+vowels = ['u', 'o', 'i', 'e', 'a']
+split_name = real_name.downcase.split(' ').reverse
+# cut up names
+first_name = split_name[0].chars
+last_name = split_name[1].chars
 
-vowels = ['a', 'e', 'i', 'o', 'u']
-
-def alias(input)
-new_name = input.downcase.split(' ').reverse
+x = 0
+first_name.length.times do
+case first_name[x]
+when "a"
+	first_name[x] = "e"
+when "e"
+	first_name[x] = "i"
+when "i"
+	first_name[x] = "o"
+when "o"
+	first_name[x] = "u"
+when "u"
+	first_name[x] = "a"
+else 
+end
+x += 1
 end
 
-puts alias(gets.chomp)
-# def vowelizer
+x = 0
+last_name.length.times do
+case last_name[x]
+when "a"
+	last_name[x] = "e"
+when "e"
+	last_name[x] = "i"
+when "i"
+	last_name[x] = "o"
+when "o"
+	last_name[x] = "u"
+when "u"
+	last_name[x] = "a"
+else 
+end
+x += 1
+end
 
-# end
 
-# puts alias(normal_name)
-# first_name = new_name[0].chars
-# last_name = new_name[1].chars
-# puts first_name
-# puts last_name
 
+p first_name
+p last_name
+p split_name
 # p first_name
 # p last_name
