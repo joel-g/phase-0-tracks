58dea1ae1a57ef667f2802575376cdd63fe5a71c
Joel Guerra <joelaguerra1@gmail.com>
2017-04-26 17:09:33 -0700

    commenting out stuff

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,22 +14,23 @@
 
 #capitalize
 
-name = gets.chomp
+
 vowels = ['a', 'e', 'i', 'o', 'u']
 
-def alias
-name = name.downcase.split(' ').reverse
-first = name[0].chars
-last = name[1].chars
-
-end
+def alias(input)
+new_name = input.downcase.split(' ').reverse
 end
 
-def vowelizer
+puts alias(gets.chomp)
+# def vowelizer
 
-end
+# end
 
+# puts alias(normal_name)
+# first_name = new_name[0].chars
+# last_name = new_name[1].chars
+# puts first_name
+# puts last_name
 
-
-p first
-p last
+# p first_name
+# p last_name
