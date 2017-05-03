ab389328aee9281de700c5893656f9d729bcc658
Joel Guerra <joelaguerra1@gmail.com>
2017-05-02 17:44:18 -0700

    complete puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,33 +1,55 @@
 class Puppy
-
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
-
   def speak(num)
-  	num.times do
-  		puts "Woof!"
-  	end
+    num.times do
+        puts "Woof!"
+    end
   end
-
   def roll_over
-  	puts "*rolls over*"
+    puts "*rolls over*"
   end
-
   def dog_years(num)
-  	dog_age = num * 7
-  end	
-
+    dog_age = num * 7
+  end   
   def sit
-  	puts "*sits*"
+    puts "*sits*"
+  end
+  def initialize
+    puts "Initializing new Puppy instance"
   end
 end
-
 daisy = Puppy.new
-
 daisy.fetch("tennis ball")
 daisy.speak(5)
 daisy.roll_over
 puts daisy.dog_years(2)
 daisy.sit
+class Kitten
+    def initialize
+        puts "Initializing Kitten instance ..."
+    end
+    def meow(int)
+        int.times do
+            puts "meow"
+        end
+    end
+    def scratch
+        puts "The cat scratched you"
+    end
+    def sleep
+        puts "zzz...zzz...zzz"
+    end
+end
+kittens = []
+50.times do
+    kittens << Kitten.new
+end
+#p kittens
+kittens.each do |instance|
+    instance.scratch
+    instance.meow(1)
+    instance.sleep
+end
