a9c842953358f49d2c10a1bf743c2b99af7596e3
joel-g <joelaguerra1@gmail.com>
2017-05-03 20:26:41 -0700

    add a shout module of yelling methods

---
+++ ruby/shout.rb
@@ -0,0 +1,11 @@
+module Shout
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+	def self.yell_happily(words)
+		words + "!!!" + "  :)"
+	end
+end
+
+p Shout.yell_happily("This is a happy test")
+p Shout.yell_angrily("This is an angry test")
