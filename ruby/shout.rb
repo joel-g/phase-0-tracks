a9e8f8c96f7f9234bc6319aedf37ba64ced4fb42
joel-g <joelaguerra1@gmail.com>
2017-05-03 20:37:02 -0700

    add Adult and Kid classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,35 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yell_happily(words)
+# 		words + "!!!" + "  :)"
+# 	end
+# end
+
+# p Shout.yell_happily("This is a happy test")
+# p Shout.yell_angrily("This is an angry test")
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
-	def self.yell_happily(words)
+	def yell_happily(words)
 		words + "!!!" + "  :)"
 	end
 end
 
-p Shout.yell_happily("This is a happy test")
-p Shout.yell_angrily("This is an angry test")
+class Kid
+	include Shout
+end
+
+class Adult
+	include Shout
+end
+
+
+diego = Kid.new
+dad = Adult.new
+
+p diego.yell_angrily("I'm so angry!")
+p dad.yell_happily("I'm so happy!")
