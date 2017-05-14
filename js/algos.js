8f6545d67027ddd5a570bc1655d966f8fa7cfa3e
Joel Guerra <joelaguerra1@gmail.com>
2017-05-13 21:04:11 -0700

    add random array and random string functions

--- js/algos.js
+++ js/algos.js
@@ -61,3 +61,47 @@ return values1
 matches(animal1, animal2)
 // console.log(keys1)
 console.log(matches(animal1, animal2))
+
+
+// # RELEASE 2
+// Function that takes an argument
+// Start with an empty array
+// Create non-sense words with a random number generator
+// Loop the creation of non-sense words to match the argument
+// push non-sense words to the array
+// return the array
+
+function randString() { // This function makes ONE word of a random length)
+	var newString = "";
+	var stringLength = Math.floor((Math.random() * 10) + 1); // generates a number between 1 and 10
+	var alpha = "abcdefghijklmnopqrstuvwxyz";
+	for (var s=0; s < stringLength; s++){
+//	while (counter < stringLength){
+		newString += alpha.charAt(Math.floor(Math.random() * alpha.length)); // populates string with random letters from ALPHA
+			}
+	return newString
+}
+
+// console.log(randString())
+
+function randArray(x) { // This function takes a parameter and calls the previous function that many times to fill the array
+	var arr = []
+	var counter = 0
+	while (counter < x){
+		arr.push(randString())
+		counter++
+	}
+	return arr
+}
+
+//Add driver code that does the following 10 times:
+// generates an array, prints the array, feeds the array to your "longest word" function, and prints the result.
+
+driverCounter = 0
+while (driverCounter < 10){
+	var workingArray; // storing array so I can run operations on it
+	workingArray = randArray(3);
+	console.log(workingArray);
+	console.log(longestStr(workingArray));
+		driverCounter++;
+}
