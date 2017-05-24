8c54899f8b299d9c5cf698c460a76e19ed58e6b6
Joel Guerra <joelaguerra1@gmail.com>
2017-05-23 17:08:19 -0700

    finish

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,10 +1,29 @@
 // alert("Test")
 
-var i;
+var header = document.getElementsByTagName("h1");
+var list = document.getElementsByTagName("ul");
+// var kid = document.getElementById("kid");
+var kid = document.getElementsByTagName("img");
 
-var list = document.getElementsByTagName("h1");
 
-var ul = list[0]
-ul.style.border="3px solid red";
+// list[0].style.visibility = "hidden";
+header[0].style.border="3px solid red";
 
-console.log(list)
+// kid2.style.border="3px solid red"
+kid[0].style.visibility = "hidden";
+
+
+function hide(event) {
+	event.target.style.visibility = "hidden";
+};
+
+function unhideKid() {
+	kid[0].style.visibility = "visible";
+	button[0].style.visibility = 'hidden';
+
+};
+var button = document.getElementsByTagName("button")
+button[0].addEventListener("click", unhideKid);
+
+list[0].addEventListener("click", hide);
+// kid[0].addEventListener("click", unhide);
