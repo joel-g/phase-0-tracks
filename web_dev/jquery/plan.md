435a16b2d2e5565f21df7bfe9943680601f206c4
Joel Guerra <joelaguerra1@gmail.com>
2017-05-24 20:10:09 -0700

    create files for shaper mover

---
+++ web_dev/jquery/plan.md
@@ -0,0 +1,5 @@
+# PLAN TO COMPLETELY MASTER JQUEREY IN 2 HOURS:
+
+Read chapter 7 of "Javascript & jQuery"
+Peruse the jQuery documentation
+Watch a youtube intro tutorial on jQuery
---
+++ web_dev/jquery/testing_script.js
@@ -0,0 +1,21 @@
+// $("document").ready(function(){
+
+$("#list li:nth-child(3)").css("color", "red");
+$("#wrapper").css("width", 1500);
+$("#wrapper").css("margin", "auto");
+// $("logo2").css();
+$("#p_two").css("color", "purple");
+$("p > em").css("color", "green");
+// $("#p_two").css{(
+// 	"color", "purple")};
+
+$("a").css("color", "red");
+$("#tableData tr:even").css("background-color", "green");
+$("#tableData tr:odd").css("background-color", "orange");
+
+var header = document.getElementsByTagName("h1");
+header[0].style.border="3px solid red";
+
+
+
+// )];
